c80de63299088dc565d7550fb06beb907d1e1733
Takahashi Takuya <takahashitakuya031126@gmail.com>
2020-06-27 10:24:51 +0900

    Loader

--- resources/js/components/PhotoForm.vue
+++ resources/js/components/PhotoForm.vue
@@ -1,7 +1,10 @@
 <template>
   <div v-show="value" class="photo-form">
     <h2 class="title">Submit a photo</h2>
-    <form class="form" @submit.prevent="submit">
+    <div v-show="loading" class="panel">
+      <Loader>Sending your photo...</Loader>
+    </div>
+    <form v-show="! loading" class="form" @submit.prevent="submit">
       <div class="errors" v-if="errors">
         <ul v-if="errors.photo">
           <li v-for="msg in errors.photo" :key="msg">{{ msg }}</li>
@@ -21,8 +24,12 @@
 
 <script>
 import { CREATED, UNPROCESSABLE_ENTITY } from '../util'
+import Loader from './Loader.vue'
 
 export default {
+  components: {
+    Loader
+  },
   props: {
     value: {
       type: Boolean,
@@ -31,6 +38,7 @@ export default {
   },
   data () {
     return {
+      loading: false,
       preview: null,
       photo: null,
       errors: null
@@ -64,9 +72,13 @@ export default {
       this.$el.querySelector('input[type="file"]').value = null
     },
     async submit () {
+      this.loading = true
+
       const formData = new FormData()
       formData.append('photo', this.photo)
       const response = await axios.post('/api/photos', formData)
+
+      this.loading = false
 
       if (response.status === UNPROCESSABLE_ENTITY) {
           this.errors = response.data.errors
