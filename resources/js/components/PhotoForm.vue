e3165fb50003b2fc1ce6a9c95f0962be2587aedc
Takahashi Takuya <takahashitakuya031126@gmail.com>
2020-06-27 10:31:37 +0900

    Message

--- resources/js/components/PhotoForm.vue
+++ resources/js/components/PhotoForm.vue
@@ -93,6 +93,11 @@ export default {
           return false
       }
 
+      this.$store.commit('message/setContent', {
+        content: '写真が投稿されました！',
+        timeout: 6000
+      })
+
       this.$router.push(`/photos/${response.data.id}`)
     }
   }
